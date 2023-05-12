a14e4a558f948e76a7fb8655e025c8911f80340c
stellafaily <lee5783611@naver.com>
2023-05-13 01:48:49 +0900

    commit1

---
+++ app/src/main/java/com/example/voicetotext/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.voicetotext;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public abstract class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn;
+    TextView tv;
+    Intent intent;
+    SpeechRecognizer mRecognizer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        tv = (TextView) findViewById(R.id.tv);
+        btn.setOnClickListener(this);
+
+
+
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
+
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mRecognizer.setRecognitionListener(recognitionListener);
+    }
+
+    public void onclick(View view) {
+        btn.setText("말 하는 중.....");
+        mRecognizer.startListening(intent);
+    }
+
+    private RecognitionListener recognitionListener = new RecognitionListener() {
+
+
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+            btn.setText("음성 인식 대기중.....");
+            tv.setText("");
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            btn.setText("버튼을 누르고 말하세요");
+        }
+
+        @Override
+        public void onError(int i) {
+            tv.setText("말이 없네요");
+            btn.setText("버튼을 누르고 말하세요");
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+            ArrayList<String> mResult = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            String[] rs = new String[mResult.size()];
+            mResult.toArray(rs);
+            tv.setText(rs[0]);
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+
+        }
+    };
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1) {
+            if(grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(),"이 앱을 실행하려면 RECORD_AUDIO 권한이 필요!",Toast.LENGTH_LONG).show();
+
+                finish();
+            }
+        }
+    }
+}
